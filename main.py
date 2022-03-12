4838555291699acc9404162b9862602d555d4b28
arx8x <arx8@icloud.com>
2022-03-12 11:21:27 +0530

    media conversion to mp4

--- main.py
+++ main.py
@@ -2,10 +2,11 @@ import json
 import requests
 from pprint import pprint as pprint
 from os import makedirs as mkdirs
-from os.path import exists as file_exists, getsize
-from utils import log, download_file, get_remote_filesize, url_split
+from os.path import exists as file_exists, getsize, basename
+from utils import log, download_file, get_remote_filesize, url_split, \
+                  replace_extension
 from validators import url as urlvalidate
-import sys
+import sys, subprocess
 
 class CL4Archiver:
     def __init__(self, url):
@@ -39,7 +40,7 @@ class CL4Archiver:
         json_data = json.loads(api_data)
         posts = json_data['posts']
         for post in posts:
-            media_url = self.__download_media(post)
+            media_url = self.__download_media(post, True)
 
 
     def __path_for_thread(self):
@@ -47,7 +48,7 @@ class CL4Archiver:
         mkdirs(path, exist_ok=True)
         return path
 
-    def __download_media(self, post) -> str:
+    def __download_media(self, post, convert_media: bool) -> str:
         if not (ext := post.get('ext')) or not (name := post.get('tim')):
             return None
         should_download_file = True
@@ -56,18 +57,35 @@ class CL4Archiver:
         log(f"Media {filename} for post {post.get('no')}")
         path = f"{self.path}/{filename}"
         if file_exists(path):
-            print_message = "Local file exists and is complete"
+            print_message = "Local file exists"
             local_size = getsize(path)
             remote_size = get_remote_filesize(url)
             if local_size != remote_size:
                 print_message += " but sizes are different (remote: %s vs local: %s)" % (remote_size, local_size)
             else:
                 should_download_file = False
+                print_message += " and is complete"
             log(print_message)
         if should_download_file:
             log(f"Downloading...")
-            download_file(url, path)
+            if not download_file(url, path):
+                log("Download failed")
+                return
+        if convert_media:
+            self.__convert_media(path)
         return
+
+    def __convert_media(self, media_path: str):
+        target_path = replace_extension(media_path, 'mp4')
+        if file_exists(target_path):
+            log("file already converted")
+            return
+        log(f"converting {basename(media_path)}...")
+        command_args = ["ffmpeg", "-i", media_path, target_path]
+        proc = subprocess.run(command_args, capture_output=True)
+        if proc.returncode:
+            log("conversion failed", 4)
+
 
 url = sys.argv.pop()
 
