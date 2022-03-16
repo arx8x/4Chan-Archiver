fba8d39f9480036d58849d833f507b8afe6153b8
arx8x <arx8@icloud.com>
2022-03-16 17:13:37 +0530

    add option to update archived threads

--- main.py
+++ main.py
@@ -3,19 +3,52 @@ import getopt
 from cl4archiver import CL4Archiver
 from utils import log
 from validators import url as urlvalidate
+from os import listdir
+from os.path import isdir, exists as path_exists
+
+convert = True
+binpath = None
+output_path = 'archives'
+
+
+def update_threads():
+    log(" >>> running updater")
+    global convert, binpath, output_path
+    board_dirs = listdir(output_path)
+    for board in board_dirs:
+        board_dir = f"{output_path}/{board}"
+        # skip hidden directories and . and ..
+        if not isdir(board_dir) or board[0] == '.':
+            continue
+        thread_dirs = listdir(board_dir)
+        for thread in thread_dirs:
+            if not thread.isnumeric():
+                log(f"\"{thread}\" isn't a valid thread id")
+                continue
+            thread_dir = f"{board_dir}/{thread}"
+            if not isdir(thread_dir) or thread[0] == '.':
+                # skip hidden directories and . and ..
+                continue
+            log(f" >>> checking {thread} from /{board}/")
+            meta_file_path = f"{thread_dir}/meta"
+            if not path_exists(meta_file_path):
+                log(" >>> thread has no initial data to update")
+                continue
+            cl4 = CL4Archiver(board, thread, binpath, output_path)
+            cl4.archive(convert)
 
 
 def main():
     try:
-        longopts = ["no-convert", "binpath=", 'output=']
-        args = getopt.getopt(sys.argv[1:], 'nbo:', longopts)
+        longopts = ["no-convert", "binpath=", 'output=', 'udpate']
+        args = getopt.getopt(sys.argv[1:], 'unbo:', longopts)
     except getopt.GetoptError as e:
         log(e.msg, 4)
         sys.exit(-1)
 
-    convert = True
-    binpath = None
-    output_path = 'archives'
+    global convert, binpath, output_path
+    update = False
+    convert = None
 
     for opt in args[0]:
         if opt[0] in ['--no-convert', '-n']:
@@ -24,8 +57,12 @@ def main():
             binpath = opt[1]
         elif opt[0] in ['--output', '-o']:
             output_path = opt[1]
-
-    if args[1]:
+        elif opt[0] in ['--update', '-u']:
+            update = True
+    if update:
+        update_threads()
+        sys.exit()
+    elif args[1]:
         thread_url = args[1].pop()
         if not thread_url or not urlvalidate(thread_url):
             log("No thread url provided", 4)
