320f7fa65dcee5884be48d774c41c7081d26480a
arx8x <arx8@icloud.com>
2022-03-17 06:58:55 +0530

    update utils - use requests module and send proper head request to hosts to get heders - make log not capture unnecessary exceptions - replace_extension can now remove extension - add intervalcheck()

--- utils.py
+++ utils.py
@@ -3,6 +3,8 @@ import urllib.request
 from urllib.error import HTTPError
 from urllib.parse import urlparse, parse_qs
 from collections import namedtuple
+import requests
+import time
 
 URLInfo = namedtuple(
     "URLInfo", ['url', 'domain', 'components', 'scheme', 'query', 'fragment'],
@@ -18,9 +20,41 @@ def log(entry, type=0):
     symbols = ['*', '+', '-', '!', '#', '>', '<']
     try:
         symbol = symbols[type]
-    except:
+    except Exception:
         symbol = '*'
     print(f"[{symbol}] {entry}")
+
+
+def intervalcheck(key: str, duration: int, use_key_as_path: bool = False,
+                  retouch: bool = True) -> bool:
+    '''
+    Checks if the given duration has passed for the given key.
+    If the duration has passed, reset the duration
+    '''
+    expired = False
+    if use_key_as_path:
+        path = key
+        dir = os.path.dirname(path)
+    else:
+        dir = 'intervalcheck'
+        path = f"{dir}/{key}"
+    os.makedirs(dir, exist_ok=True)
+    if os.path.exists(path):
+        last_modified = os.path.getmtime(path)
+        current_time = time.time()
+        delta = current_time - last_modified
+        if delta > duration:
+            expired = True
+    else:
+        expired = True
+
+    # update the last_modified if needed
+    if expired and retouch:
+        with open(path, 'w+') as f:
+            f.write('')
+        return True
+    return expired
+
 
 def get_redirect_url(url):
     # urllib.request follows  rediects automatically
@@ -48,9 +82,9 @@ def get_remote_filesize(url):
     if not url:
         return None
     try:
-        r = urllib.request.urlopen(url)
-        header_info = r.info()
-        if (size := header_info.get('content-length')):
+        r = requests.head(url)
+        headers = r.headers
+        if (size := headers.get('content-length')):
             return int(size)
     except Exception as e:
         print(e)
@@ -83,10 +117,13 @@ def url_split(url):
     return pathinfo
 
 
-def replace_extension(path, extension):
-    if not path or not extension:
+def replace_extension(path, extension=None):
+    if not path:
         return None
     path_split = os.path.splitext(path)
+    # if no extension is given, remove extension
+    if not extension:
+        return path_split[0]
     if not path_split:
         return None
     new_path = f"{path_split[0]}.{extension}"
